e6e1879f9159c80407c942416cee0872629ddb1f
Troy Swingler <tswingler5@gmail.com>
2026-02-23 16:28:44 +0100

    Auto formatting

--- utils.js
+++ utils.js
@@ -1,28 +1,28 @@
 export async function getActiveTabURL() {
-    const queryOptions = { active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
-    // [tab] gets first element of the array returned by query
-    const [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
+  const queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  // [tab] gets first element of the array returned by query
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
 }
 
 
 export async function fetchParsedProductList() {
-    const productsData = await chrome.storage.sync.get(null);
-    // parse all stored products from JSON strings to objects
-    Object.keys(productsData).forEach(key => {
-        productsData[key] = JSON.parse(productsData[key]);
-    });
-    return productsData;
+  const productsData = await chrome.storage.sync.get(null);
+  // parse all stored products from JSON strings to objects
+  Object.keys(productsData).forEach(key => {
+    productsData[key] = JSON.parse(productsData[key]);
+  });
+  return productsData;
 }
 
 
 export async function fetchProduct(productID) {
-    const productData = await chrome.storage.sync.get(productID);
-    return productData[productID] ? JSON.parse(data[productID]) : {};
-    // return new Promise((resolve) => {
-    //     chrome.storage.sync.get([productID], (data) => {
-    //         resolve(data[productID] ? JSON.parse(data[productID]) : {})
-    //     })
-    // })
+  const productData = await chrome.storage.sync.get(productID);
+  return productData[productID] ? JSON.parse(data[productID]) : {};
+  // return new Promise((resolve) => {
+  //     chrome.storage.sync.get([productID], (data) => {
+  //         resolve(data[productID] ? JSON.parse(data[productID]) : {})
+  //     })
+  // })
 }
